f98be9aa2ab3b46cc2aca22d1342826847d9bc9a
adaminthecape <adaminthecape@gmail.com>
2025-03-10 13:56:11 +0200

    Create GenericDatabase.ts
    Interface for handling very generic db operations. Non-functional by design. To be extended by other db handlers.

---
+++ src/models/Database/GenericDatabase.ts
@@ -0,0 +1,121 @@
+import { DbFilters } from './DbFilters';
+import { DbPaginationOpts, PaginatedItemResponse } from './Pagination';
+import { Item } from '../Items/GenericItem';
+
+export type GenericDatabaseOpts = {
+	isDebugMode?: boolean;
+};
+export class GenericDatabase<
+	IItemIdType extends (string | number) = string,
+	IItemType = Item
+>
+{
+	protected isDebugMode?: boolean;
+
+	constructor(opts: GenericDatabaseOpts)
+	{
+		this.isDebugMode = !!opts.isDebugMode;
+	}
+
+	protected async getDb(): Promise<any | undefined>
+	{
+		return undefined;
+	}
+
+	/** @deprecated */
+	public async watch(opts: {
+		rootPath: string;
+		withResult: (result: any) => void;
+	})
+	{
+		//
+	}
+
+	public async update<T = Record<string, unknown>>(opts: {
+		itemId: string;
+		itemType: string;
+		path?: string | undefined;
+		data: IItemType;
+		setUpdated?: boolean;
+	})
+	{
+		//
+	}
+
+	public async updateMultiple(opts: {
+		items: Record<string, IItemType>;
+		itemType: string;
+	})
+	{
+		//
+	}
+
+	public async insert(opts: {
+		itemId: string;
+		itemType: string;
+		data: IItemType;
+	}): Promise<void>
+	{
+		//
+	}
+
+	public async insertMultiple(opts: {
+		itemType: string;
+		items: Record<string, IItemType>;
+	}): Promise<void>
+	{
+		//
+	}
+
+	/** @deprecated */
+	public async select1r(opts: {
+		itemId: string;
+		itemType: string;
+	}): Promise<IItemType | undefined>
+	{
+		return {} as IItemType;
+	}
+
+	public async select<T = Record<string, unknown>>(opts: {
+		itemId: string;
+		itemType: string;
+		filters?: DbFilters;
+	}): Promise<IItemType | undefined>
+	{
+		return {} as IItemType;
+	}
+
+	public async selectMultiple<T = Record<string, unknown>>(opts: {
+		itemType: string;
+		itemIds?: string[] | undefined;
+		filters?: DbFilters;
+		pagination?: DbPaginationOpts;
+	}): Promise<PaginatedItemResponse<IItemType>>
+	{
+		return {
+			results: [] as Array<IItemType>,
+			hasMore: false,
+			totalItems: 0,
+			pagination: {
+				page: 1,
+				pageSize: 0
+			}
+		};
+	}
+
+	public async remove(opts: {
+		itemId: string;
+		itemType: string;
+	}): Promise<void>
+	{
+		//
+	}
+
+	public async removeMultiple(opts: {
+		itemIds: Array<string>;
+		itemType: string;
+	}): Promise<void>
+	{
+		//
+	}
+}
